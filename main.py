f76a438fe82490ca0bd89910fd50ae56e976ee87
0marsm0 <alish.yuldashev88@gmail.com>
2025-05-16 15:11:21 +0200

    add pages, .xlsx for page 4

---
+++ main.py
@@ -0,0 +1,21 @@
+from taipy.gui import Gui
+from frontend.pages.home import home_page
+from frontend.pages.page_1 import page_1
+from frontend.pages.page_2 import page_2
+from frontend.pages.page_3 import page_3
+from frontend.pages.page_4 import page_4
+
+
+pages = {
+    "/home": home_page,
+    "/page_1": page_1,
+    "/page_2": page_2,
+    "/page_3": page_3,
+    "/page_4": page_4,
+}
+
+
+if __name__ == "__main__":
+    Gui(pages=pages).run(
+        dark_mode=False, title="The Skool Dashboard", use_reloader=True, port=8080
+    )
